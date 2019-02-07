71ec567b9ee25aa1d3c19ae6e83a0e6dd080510b
NyaHo-vrc <47382748+NyaHo-vrc@users.noreply.github.com>
2019-02-07 11:10:30 +0900

    first commit

---
+++ dailyFolder/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace dailyFolder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtBox_targetPath = new System.Windows.Forms.TextBox();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.btn_SelectFolder = new System.Windows.Forms.Button();
+            this.textArea = new System.Windows.Forms.TextBox();
+            this.btn_Start = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtBox_targetPath
+            // 
+            this.txtBox_targetPath.Location = new System.Drawing.Point(106, 13);
+            this.txtBox_targetPath.Name = "txtBox_targetPath";
+            this.txtBox_targetPath.ReadOnly = true;
+            this.txtBox_targetPath.Size = new System.Drawing.Size(495, 19);
+            this.txtBox_targetPath.TabIndex = 0;
+            this.txtBox_targetPath.WordWrap = false;
+            // 
+            // folderBrowserDialog1
+            // 
+            this.folderBrowserDialog1.Description = "処理の対象となるフォルダを選択してください。";
+            this.folderBrowserDialog1.ShowNewFolderButton = false;
+            // 
+            // btn_SelectFolder
+            // 
+            this.btn_SelectFolder.Location = new System.Drawing.Point(13, 13);
+            this.btn_SelectFolder.Name = "btn_SelectFolder";
+            this.btn_SelectFolder.Size = new System.Drawing.Size(87, 23);
+            this.btn_SelectFolder.TabIndex = 1;
+            this.btn_SelectFolder.Text = "フォルダを選択";
+            this.btn_SelectFolder.UseVisualStyleBackColor = true;
+            this.btn_SelectFolder.Click += new System.EventHandler(this.btn_SelectFolder_Click);
+            // 
+            // textArea
+            // 
+            this.textArea.Location = new System.Drawing.Point(13, 43);
+            this.textArea.Multiline = true;
+            this.textArea.Name = "textArea";
+            this.textArea.ReadOnly = true;
+            this.textArea.ScrollBars = System.Windows.Forms.ScrollBars.Both;
+            this.textArea.Size = new System.Drawing.Size(588, 395);
+            this.textArea.TabIndex = 2;
+            this.textArea.WordWrap = false;
+            // 
+            // btn_Start
+            // 
+            this.btn_Start.Enabled = false;
+            this.btn_Start.Location = new System.Drawing.Point(13, 445);
+            this.btn_Start.Name = "btn_Start";
+            this.btn_Start.Size = new System.Drawing.Size(75, 23);
+            this.btn_Start.TabIndex = 3;
+            this.btn_Start.Text = "Start";
+            this.btn_Start.UseVisualStyleBackColor = true;
+            this.btn_Start.Click += new System.EventHandler(this.btn_Start_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(616, 477);
+            this.Controls.Add(this.btn_Start);
+            this.Controls.Add(this.textArea);
+            this.Controls.Add(this.btn_SelectFolder);
+            this.Controls.Add(this.txtBox_targetPath);
+            this.Name = "Form1";
+            this.Text = "DailyFolder";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtBox_targetPath;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.Button btn_SelectFolder;
+        private System.Windows.Forms.TextBox textArea;
+        private System.Windows.Forms.Button btn_Start;
+    }
+}
+
---
+++ dailyFolder/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace dailyFolder
+{
+    public partial class Form1 : Form
+    {
+        const int WM_GETTEXTLENGTH = 0x000E;
+        const int EM_SETSEL = 0x00B1;
+        const int EM_REPLACESEL = 0x00C2;
+
+        List<string[]> taskList = new List<string[]>();
+
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, string lParam);
+
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int uMsg, int wParam, int lParam);
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_SelectFolder_Click(object sender, EventArgs e)
+        {
+            //フォルダ選択ダイアログがキャンセルされた場合、何もしない
+            if(folderBrowserDialog1.ShowDialog() != DialogResult.OK){
+                return;
+            }
+            taskList.Clear();
+            txtBox_targetPath.Text = folderBrowserDialog1.SelectedPath;
+            btn_Start.Enabled = true;
+
+            textArea.Text = "";
+            SendMessage(textArea.Handle, EM_REPLACESEL, 1, "以下の処理が実行されます。\r\n");
+            SendMessage(textArea.Handle, EM_REPLACESEL, 1, "----------------------------------------\r\n");
+
+            var fileList = Directory.EnumerateFiles(folderBrowserDialog1.SelectedPath,"*");
+            if (!fileList.Any())
+            {
+                SendMessage(textArea.Handle, EM_REPLACESEL, 1, "対象ファイルなし\r\n");
+                btn_Start.Enabled = false;
+                return;
+            }
+
+            foreach (var filePath in fileList)
+            {
+                var fileAttr = File.GetAttributes(filePath);
+                //隠しファイルは対象外
+                if ((fileAttr & FileAttributes.Hidden) == FileAttributes.Hidden)
+                {
+                    continue;
+                }
+
+                var fileName = Path.GetFileName(filePath);
+                var createDate = File.GetCreationTime(filePath).ToString("yyyyMMdd");
+
+                var job = new string[] { fileName, createDate };
+                taskList.Add(job);
+
+                SendMessage(textArea.Handle, EM_REPLACESEL, 1, fileName +"　が　" + createDate + "　フォルダへ移動します。\r\n");
+            }
+
+            SendMessage(textArea.Handle, EM_REPLACESEL, 1, "----------------------------------------\r\n");
+            SendMessage(textArea.Handle, EM_REPLACESEL, 1, "Start ボタンをクリックし、処理を実行してください。\r\n");
+        }
+
+        private void btn_Start_Click(object sender, EventArgs e)
+        {
+            btn_Start.Enabled = false;
+            SendMessage(textArea.Handle, EM_REPLACESEL, 1, "\r\n\r\n\r\n");
+            SendMessage(textArea.Handle,EM_REPLACESEL,1,"ファイルの移動を実行します。\r\n");
+            SendMessage(textArea.Handle, EM_REPLACESEL, 1, "----------------------------------------\r\n");
+
+            foreach (var job in taskList)
+            {
+                if (!Directory.Exists(folderBrowserDialog1.SelectedPath + "\\" + job[1]))
+                {
+                    Directory.CreateDirectory(folderBrowserDialog1.SelectedPath + "\\" + job[1]);
+                    SendMessage(textArea.Handle, EM_REPLACESEL, 1, job[1] + "　フォルダを作成しました。\r\n");
+                }
+                try
+                {
+                    File.Move(folderBrowserDialog1.SelectedPath + "\\" + job[0], folderBrowserDialog1.SelectedPath + "\\" + job[1] + "\\" + job[0]);
+                    SendMessage(textArea.Handle, EM_REPLACESEL, 1, job[0] + "　を　" + job[1] + "　フォルダへ移動しました。\r\n");
+                }
+                catch (IOException)
+                {
+                    SendMessage(textArea.Handle, EM_REPLACESEL, 1, job[0] + "　が既に移動先へ存在します。ファイルの移動をスキップします。\r\n");
+                }
+            }
+            taskList.Clear();
+            SendMessage(textArea.Handle, EM_REPLACESEL, 1, "----------------------------------------\r\n");
+            SendMessage(textArea.Handle, EM_REPLACESEL, 1, "処理が完了しました。\r\n");
+        }
+    }
+}
